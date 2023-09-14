b9888ab0d40f6a45f459f375bd181a2e5d37f9e5
arkenstan <anshulwagadre@gmail.com>
2023-09-14 17:56:10 +0530

    Initial commit

---
+++ apps/room-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dapp-p2p-gun-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'room-ui';
+}
